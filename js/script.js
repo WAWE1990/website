c153d50b36c31819aeaaba28af8c8e7575d4686a
Uday Datar <88326329+WAWE1990@users.noreply.github.com>
2021-08-02 18:18:36 +0530

    w

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var vara = new Vara("#container", "https://rawcdn.githack.com/akzhy/Vara/ed6ab92fdf196596266ae76867c415fa659eb348/fonts/Satisfy/SatisfySL.json", [{
+  text: "Hello World!!",
+  fontSize: 48,
+  y:10
+}, {
+  text: "Realistic Animations",
+  fontSize: 34,
+  color:"#f44336"
+}], {
+  strokeWidth: 2,
+  textAlign:"center"
+});
